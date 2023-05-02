b365bae12a6be4bf4c9eac140e0fe5176110c9d7
Bram Colleman <bram.colleman@telenet.be>
2023-05-03 01:15:04 +0200

    feat: add register function

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+
+require("dotenv").config();
 
 mongoose.set('strictQuery', false);
 
 mongoose.connect(process.env.DB_CONNECTION);
 
-// const adminsRouter = require("./routes/admins");
 const eldercareRouter = require("./routes/eldercare");
 
 const app = express();
@@ -32,7 +32,7 @@ app.use(
 );
 
 
-// app.use("/api/v1/admins", adminsRouter);
+app.use("/api/eldercare", eldercareRouter);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
