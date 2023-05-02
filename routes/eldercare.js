f71f5a9308fe3d71a99bc0f7d39008faf4bbd786
Senne <>
2023-05-03 00:22:51 +0200

    feat: add eldercare controller and dependencies

---
+++ routes/eldercare.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const eldercareController = require("../controllers/eldercare");
+
+router.post("/create", eldercareController.create);
+router.post("/login", eldercareController.login);
+
+
+module.exports = router;
