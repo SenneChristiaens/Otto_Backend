2b7c044ef48d78d19f236a51da5bd6d6b00f4e91
Bram-Colleman <r0820751@student.thomasmore.be>
2023-05-03 12:05:56 +0200

    fix: fix login response v2

--- controllers/drivers.js
+++ controllers/drivers.js
@@ -67,7 +67,7 @@ const login = async (req, res) => {
       res.json({
         status: "success",
         token: token,
-        name: driver.givenName + " " + driver.lastName,
+        name: driver.givenName + " " + driver.familyName,
       });
     } else {
       res.json({
