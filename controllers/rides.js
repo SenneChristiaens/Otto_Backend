167b8ae7ae4eb32e775af8d5c78df5788149b0b2
Bram-Colleman <r0820751@student.thomasmore.be>
2023-06-06 18:09:39 +0200

    feat: change create ride

--- controllers/rides.js
+++ controllers/rides.js
@@ -6,8 +6,10 @@ const jwt = require("jsonwebtoken");
 
 const create = async (req, res) => {
     let ride = new Ride();
-    ride.origin = [req.body.origin.lat, req.body.origin.lng];
-    ride.destination = [req.body.destination.lat, req.body.destination.lng];
+    ride.origin = req.body.origin;
+    ride.originAddress = req.body.originaddress;
+    ride.destination = req.body.destination;
+    ride.destinationAddress = req.body.destinationaddress;
     ride.driver = null;
     ride.timeStamp = new Date(req.body.timeStamp);
     
