33ac2a604105a3ee835696401230122d7b5f7ed5
Senne Christiaens <senne.christiaens@icloud.com>
2023-06-10 12:57:29 +0200

    feat: add function to edit resident

--- controllers/residents.js
+++ controllers/residents.js
@@ -20,7 +20,32 @@ const create = async (req, res) => {
     });
   };
 
+  //create function to edit resident
+
+  const edit = async (req, res) => {
+    let resident = await Resident.findById(req.params.id);
+    resident.name = req.body.name;
+    resident.dateOfBirth = req.body.dateOfBirth;
+    resident.roomNumber = req.body.roomNumber;
+    resident.emergencyContact = req.body.emergencyContact;
+    resident.needs = req.body.needs;
+    resident.eldercare = req.body.eldercare;
+
+    resident.save().then(result => {
+      res.json({
+        status: "success",
+        data: {
+          msg: "Resident '" + resident.name + "' edited successfully",
+        },
+      });
+    });
+  };
+
+
+
+
   module.exports = {
     create,
+    edit,
   };
   
