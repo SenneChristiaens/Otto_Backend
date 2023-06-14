662bfab021d52b2a878bd51b34b8f0aca031e3e8
Bram Colleman <bram.colleman@telenet.be>
2023-06-14 17:04:44 +0200

    feat: add primus functionality

--- primus/primus.js
+++ primus/primus.js
@@ -2,12 +2,12 @@ const Primus = require("primus");
 
 let go = (server) => {
   let primus = new Primus(server, {});
-  //primus.save(__dirname +'/primuslib.js')
-
   primus.on("connection", (spark) => {
-    spark.on("data", (data) => {
-        console.log(data);
-        // primus.write(data);
+    console.log("connected");
+
+    spark.on('data', (data) => {
+        console.log(data.data);
+        primus.write(data);
         }
     );
   });
